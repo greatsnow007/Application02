da28482443522ea5854a0cf3a034f197ae51d52d
greatsnow007 <923516176@qq.com>
2017-07-10 11:42:12 +0800

    布局讲解

--- app/src/main/java/com/bxw/application02/MainActivity.java
+++ app/src/main/java/com/bxw/application02/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bxw.application02;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
